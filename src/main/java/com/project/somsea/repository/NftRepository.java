bfe2c81c90b49506a9058b6d5c495f919fff8ad6
Songsiyeon <asdf748596123@gmail.com>
2022-05-31 20:31:00 +0900

    add nft read service (#13)

--- src/main/java/com/project/somsea/repository/NftRepository.java
+++ src/main/java/com/project/somsea/repository/NftRepository.java
@@ -2,10 +2,16 @@ package com.project.somsea.repository;
 
 import com.project.somsea.domain.Collection;
 import com.project.somsea.domain.Nft;
+import com.project.somsea.domain.Part;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface NftRepository extends JpaRepository<Nft, Long> {
     List<Nft> findAllByCollection(Collection collection);
+
+    @Query(value = "SELECT DISTINCT n FROM Nft n JOIN FETCH n.nftInfos infos WHERE infos.part IN (:parts)")
+    List<Nft> findAllByJoinWithParts(@Param("parts") List<Part> parts);
 }
--- src/test/java/com/project/somsea/service/NftServiceTest.java
+++ src/test/java/com/project/somsea/service/NftServiceTest.java
@@ -124,50 +124,78 @@ class NftServiceTest {
         nftRepository.saveAndFlush(nft2);
 
         //when
-        List<NftDto.ResponseByCollection> nftDtos = nftService.readNftByCollection(collection.getId());
+        List<NftDto.Response> nftDtos = nftService.readNftByCollection(collection.getId());
 
         //then
-        NftDto.ResponseByCollection nftDto1 = nftDtos.get(0);
+        NftDto.Response nftDto1 = nftDtos.get(0);
         assertThat(nftDto1.getUserId()).isEqualTo(user1.getId());
-        assertThat(nftDto1.getNfTId()).isEqualTo(nft1.getId());
+        assertThat(nftDto1.getNftId()).isEqualTo(nft1.getId());
         assertThat(nftDto1.getImageUrl()).isEqualTo(nft1.getImageUrl());
         assertThat(nftDto1.getNftTitle()).isEqualTo(nft1.getTitle());
 
-        NftDto.ResponseByCollection nftDto2 = nftDtos.get(1);
+        NftDto.Response nftDto2 = nftDtos.get(1);
         assertThat(nftDto2.getUserId()).isEqualTo(user2.getId());
-        assertThat(nftDto2.getNfTId()).isEqualTo(nft2.getId());
+        assertThat(nftDto2.getNftId()).isEqualTo(nft2.getId());
         assertThat(nftDto2.getImageUrl()).isEqualTo(nft2.getImageUrl());
         assertThat(nftDto2.getNftTitle()).isEqualTo(nft2.getTitle());
     }
     @Test
     @DisplayName("Nft 디테일 조회")
     void testReadDetailNft() {
-        //given
+        //given : nft
+        Collection collection = new Collection();
+        collectionRepository.saveAndFlush(collection);
 
+        User user = new User();
+        userRepository.saveAndFlush(user);
+
+        Nft nft = Nft.builder().imageUrl("myImageUrl1").collection(collection).user(user).build();
+        nftRepository.saveAndFlush(nft);
 
         //when
+        NftDto.ResponseDetail nftDetail = nftService.readDetailNft(nft.getId());
 
-
-        //then
-
-
+        //then: 조회는 보통 DTO 값을 확인하는게 좋음
+        assertThat(nftDetail.getNftId()).isEqualTo(nft.getId());
+        assertThat(nftDetail.getTitle()).isEqualTo(nft.getTitle());
+        assertThat(nftDetail.getImageUrl()).isEqualTo(nft.getImageUrl());
+        assertThat(nftDetail.getToken()).isEqualTo(nft.getToken());
+        assertThat(nftDetail.getContractAddress()).isEqualTo(nft.getContractAddress());
+        assertThat(nftDetail.getTokenStandard()).isEqualTo(nft.getTokenStandard());
+        assertThat(nftDetail.getBlockChain()).isEqualTo(nft.getBlockChain());
+        assertThat(nftDetail.getUserId()).isEqualTo(nft.getUser().getId());
     }
 
     @Test
     @DisplayName("파츠로 Nft 조회")
     void testReadNftByParts() {
-        //given
+        //given : NFT - Part 랑 연결된 케이스, 연결 안된 케이스
+        User user = new User();
+        userRepository.saveAndFlush(user);
 
+        Nft nft1 = Nft.builder().user(user).build();
+        Nft nft2 = Nft.builder().user(user).build();
+        Nft nft3 = Nft.builder().user(user).build();
+        nftRepository.saveAllAndFlush(List.of(nft1, nft2, nft3));
 
+        Part part1 = new Part();
+        Part part2 = new Part();
+        Part part3 = new Part();
+        partRepository.saveAllAndFlush(List.of(part1, part2, part3));
+
+        NftInfo nftInfo1 = NftInfo.builder().nft(nft1).part(part1).build();
+        NftInfo nftInfo2 = NftInfo.builder().nft(nft1).part(part2).build();
+        NftInfo nftInfo3 = NftInfo.builder().nft(nft2).part(part2).build();
+        nftInfoRepository.saveAllAndFlush(List.of(nftInfo1, nftInfo2, nftInfo3));
 
         //when
-
-
-
+        List<NftDto.Response> nftDtos = nftService.readNftByParts(List.of(part1.getId(), part2.getId()));
 
         //then
-
-
+        List<Long> responseNftIds = nftDtos.stream().map(NftDto.Response::getNftId).collect(Collectors.toList());
+        assertThat(responseNftIds.size()).isEqualTo(2);    // nft1, nft2
+        assertThat(responseNftIds).contains(nft1.getId());
+        assertThat(responseNftIds).contains(nft2.getId());
     }
 }
 
