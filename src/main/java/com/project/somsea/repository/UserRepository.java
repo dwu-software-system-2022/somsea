ffe02f7c5d67a6ac944f8bc2effd36c2e3b4c139
gynhyunkim <gyeonghyun.kimm@gmail.com>
2022-06-11 22:22:25 +0900

    update: 중복가입 확인을 위해 이메일쿼리 추가

--- src/main/java/com/project/somsea/repository/UserRepository.java
+++ src/main/java/com/project/somsea/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.project.somsea.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	User findByEmail(String email);
 }
