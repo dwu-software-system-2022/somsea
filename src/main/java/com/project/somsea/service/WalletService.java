c931e73597709f22d0311d3587d11b0ce61bfcbf
gynhyunkim <gyeonghyun.kimm@gmail.com>
2022-06-17 16:39:13 +0900

    update: user 참조 삭제

--- src/main/java/com/project/somsea/service/WalletService.java
+++ src/main/java/com/project/somsea/service/WalletService.java
@@ -4,10 +4,8 @@ import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
-import com.project.somsea.domain.User;
 import com.project.somsea.domain.Wallet;
 import com.project.somsea.dto.WalletDto;
-import com.project.somsea.repository.UserRepository;
 import com.project.somsea.repository.WalletRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -16,22 +14,16 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 @RequiredArgsConstructor
 public class WalletService {
-	private final UserRepository userRepository;
 	private final WalletRepository walletRepository;
 	
-	private User findUser(Long userId) {
-		return userRepository.findById(userId)
-				.orElseThrow(() -> new IllegalArgumentException("User Id 값이 없습니다. UserId: " + userId));
-	}
 	
 	private Wallet findWallet(Long walletId) {
 		return walletRepository.findById(walletId)
 				.orElseThrow(() -> new IllegalArgumentException("Wallet Id 값이 없습니다. WalletId: " + walletId));
 	}
 	
-	public Long add(Long userId, WalletDto.Request walletDto) {
-		User user = findUser(userId);
-		Wallet wallet = walletDto.toEntity(user, Integer.toUnsignedLong(10));
+	public Long add(WalletDto.Request walletDto) {
+		Wallet wallet = walletDto.toEntity(Integer.toUnsignedLong(10));
 		walletRepository.save(wallet);
 		return wallet.getId();
 	}
