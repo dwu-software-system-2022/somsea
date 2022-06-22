5dba0baa4591e32f4f1ce8f2b203227bc3e67538
yeonsiyeon <asdf748596123@gmail.com>
2022-06-22 22:10:42 +0900

    index mapping

---
+++ src/main/java/com/project/somsea/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.project.somsea.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/index")
+    public String index() {
+        return "redirect:/";
+    }
+}
